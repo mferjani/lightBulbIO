ae587bf29311c09678f6855a1502774f71c3da07
mohammed ferjani <mohammed.ferjani@gmail.com>
2018-08-29 16:17:19 -0700

    stored calc fields

---
+++ trainingIO/lightbulbTrainingIO/src/types/Warehouse.js
@@ -0,0 +1,21 @@
+function getAggregatedValues(fields) {
+
+	var myMap = [];
+	
+	myMap['pendingArrivals'] = 0;
+	myMap['lightBulbsInUse'] = 0;
+	myMap['currentInventory'] = 0;
+	myMap['holdingCostPerDay'] = 0;
+	
+	var warehouses = Warehouse.fetch({include:'pendingArrivals, lightBulbsInUse, currentInventory, holdingCostPerDay', limit:-1}).objs;
+	
+    for (var k = 0; k < warehouses.length; k++) {
+    	myMap['pendingArrivals'] = myMap['pendingArrivals'] + warehouses[k]['pendingArrivals'];
+    	myMap['lightBulbsInUse'] = myMap['lightBulbsInUse'] + warehouses[k]['lightBulbsInUse'];
+    	myMap['currentInventory'] = myMap['currentInventory'] + warehouses[k]['currentInventory'];
+    	myMap['holdingCostPerDay'] = myMap['holdingCostPerDay'] + warehouses[k]['holdingCostPerDay'];
+    }
+    
+    return myMap;
+	  
+}
